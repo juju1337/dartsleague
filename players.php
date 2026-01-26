a0d1a5b4537eb231cd178d11feb7b2371df69d14
juju1337 <julian@lamberty.org>
2026-01-26 15:38:46 +0100

    Added link to Stat Import

--- players.php
+++ players.php
@@ -195,6 +195,7 @@ function deletePlayer($id) {
         <?php if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in']): ?>
             | <a href="players.php">Player Management</a>
             | <a href="matchday_setup.php">Tournament Setup</a>
+            | <a href="import_stats.php">Import Stats</a>
             | <a href="index.php?logout=1">Logout</a>
         <?php else: ?>
             | <a href="index.php#login">Login</a>
@@ -202,3 +203,4 @@ function deletePlayer($id) {
     </p>
 </body>
 </html>
+
